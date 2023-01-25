d3363f159346b9ffcf52cb9e462b46ce174b82b7
asorkin <asorkin@techinsights.com>
2023-01-25 16:09:58 -0500

    GUI added WIP

--- gui_test_1.py
+++ gui_test_1.py
@@ -1,37 +1,50 @@
-import tkinter
-import tkinter.messagebox
+from PIL import Image
 import customtkinter
+import tkinter
+import customtkinter
+from PIL import ImageTk, Image
+import test1
+import os
+import math
+import time
+import pandas as pd
+import numpy as np
 
-customtkinter.set_appearance_mode("Light")  # Modes: "System" (standard), "Dark", "Light"
+customtkinter.set_appearance_mode("Dark")  # Modes: "System" (standard), "Dark", "Light"
 customtkinter.set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark-blue"
 
+minsize = 0
+maxsize = 100
+mingray = 127
+maxgray = 255
+debug = True
+df = pd.DataFrame()
 
 class App(customtkinter.CTk):
-    minsize = 0
-    maxsize = 100
-    mingray = 127
-    maxgray = 255
+
+
     def __init__(self):
         super().__init__()
 
         # configure window
-        self.title("CustomTkinter complex_example.py")
-        self.geometry(f"{400}x{380}")
+        self.title("Distortion Calculator")
+        self.geometry(f"{480}x{300}")
 
         # configure grid layout (4x4)
         self.grid_columnconfigure(2, weight=1)
         #self.grid_columnconfigure((2, 3), weight=0)
         self.grid_rowconfigure((0,1), weight=1)
 
+#########################             Layout
 
         self.slider_frame = customtkinter.CTkFrame(self , border_width=2, border_color=['#86878a','#86878a'])
         self.slider_frame.grid(row=0, column=0, padx=(20, 20), pady=(20, 20), sticky="new")
         # Labels
-        self.SliderLabel = customtkinter.CTkLabel(master=self.slider_frame, text='-->',  font=('aeral',14))
+        self.SliderLabel = customtkinter.CTkLabel(master=self.slider_frame, text='',  font=('aeral',14))
         self.SliderLabel.grid(row=0,column=0, padx=(70,50), pady=(15, 5) , sticky='e' )
-        self.lblMinValue = customtkinter.CTkLabel(master=self.slider_frame, text='44',  font=('aeral',14))
+        self.lblMinValue = customtkinter.CTkLabel(master=self.slider_frame, text='',  font=('aeral',14))
         self.lblMinValue.grid(row=0,column=0, padx=(10,80), pady=(15, 5) , sticky='e' )
-        self.lblMaxValue = customtkinter.CTkLabel(master=self.slider_frame, text='123',  font=('aeral',14))
+        self.lblMaxValue = customtkinter.CTkLabel(master=self.slider_frame, text='',  font=('aeral',14))
         self.lblMaxValue.grid(row=0,column=0, padx=(120, 10), pady=(15, 5) , sticky='e' )
         self.lblMin = customtkinter.CTkLabel(master=self.slider_frame, text='min: ',  font=('aeral',14))
         self.lblMin.grid(row=1,column=0, padx=(10, 0), sticky='w' )
@@ -44,10 +57,17 @@ class App(customtkinter.CTk):
         self.slider_dia_max = customtkinter.CTkSlider(master=self.slider_frame, progress_color='#86878a', fg_color='#86878a', button_color='#86878a', state='disable',
             command=self.slider_max_event)
         self.slider_dia_max.grid(row=2, column=0, padx=(40, 10), pady=(7, 15), sticky="nw")
-        self.chkSizeLimit = customtkinter.CTkCheckBox(master=self.slider_frame, text='Size Limit', command=self.chkbox_SizeLimit)
+        self.chkSizeLimit = customtkinter.CTkCheckBox(master=self.slider_frame, text='Size Limit', command=self.chkbox_SizeLimit, state='disabled')
         self.chkSizeLimit.grid(row=0, column=0, pady=(20, 10), padx=(10,0), sticky="nw")
-        self.LoadImage = customtkinter.CTkButton(master = self)
-        self.LoadImage.pack(row=1,column=0, padx=10, pady=40, text="Load Image")
+        self.btn_LoadImage = customtkinter.CTkButton(master=self, text="Load Image", command=self.load_image)
+        self.btn_LoadImage.grid(row=0,column=1, padx=10, pady=(40,0), sticky='n')
+        self.btn_DetectViaas = customtkinter.CTkButton(master=self, text="Detect", command=self.detect_vias, state='disabled')
+        self.btn_DetectViaas.grid(row=0,column=1, padx=10, pady=(100, 0), sticky='n')
+        self.btn_Calculate = customtkinter.CTkButton(master=self, text="Calculate", command=self.calculate, state='disabled')
+        self.btn_Calculate.grid(row=0,column=1, padx=10, pady=(160, 0) , sticky='n')
+        self.btn_Submit = customtkinter.CTkButton(master=self, text="Submit", command=self.submit, state='disabled')
+        self.btn_Submit.grid(row=0,column=1, padx=10, pady=(220, 0) , sticky='n')        
+
  # from web Example create checkbox and switch frame
         # self.checkbox_slider_frame = customtkinter.CTkFrame(self)
         # self.checkbox_slider_frame.grid(row=1, column=1, padx=(20, 20), pady=(20, 0), sticky="ew")
@@ -59,13 +79,58 @@ class App(customtkinter.CTk):
         # self.switch_2 = customtkinter.CTkSwitch(master=self.checkbox_slider_frame)
         # self.switch_2.grid(row=4, column=0, pady=(10, 20), padx=20, sticky="n")
 
+#########################             Control
+    def calculate(self):
+        #rows_list = []    
+        rows_list, rows_vias_cnt = test1.data_process(df)
+        cnt_vias_in_row = max(rows_vias_cnt.keys())
+    #--- Check if there is different vias count in the rows and choose the largest vias count
+        if len(rows_vias_cnt)>1:
+            #cnt_vias_in_row = max(rows_vias_cnt, key=rows_vias_cnt.get)
+            test1.pymsgbox.alert(f'There are rows with diferent vias counts! \n vias_num:rows_count {rows_vias_cnt}' +
+            '\n Only rows with largest vias count will be selected', 'Title')
+        poly_args = test1.plot_distortions(rows_list)        
 
+    def submit(self):
+        pass
+
+    def detect_vias(self):
+        global df
+        dia = self.slider_dia_min.get()
+        minarea = math.pi * dia * dia / 4
+        dia = self.slider_dia_max.get()
+        maxarea = math.pi * dia * dia / 4
+        df = test1.regen_image(minarea, maxarea)
+        self.btn_Calculate.configure(state='normal')
+        if (debug):
+            print(df)
+
+    def load_image(self): 
+        mymin,mymean,mymax = test1.find_min_max_size()
+        if((mymin!=0) & (mymax!=0)):
+            minsize = math.ceil(math.sqrt(4*mymin/math.pi))
+            minsize = minsize*0.9 # reduce 10%
+            maxsize = math.ceil(math.sqrt(4*mymax/math.pi))
+            maxsize = maxsize*1.1 # increase 10%
+            avgsize = (minsize + int(maxsize*0.1))/2
+            self.slider_dia_min.configure(from_=minsize)
+            self.slider_dia_min.configure(to=maxsize)
+            self.slider_dia_min.set(avgsize)
+            self.lblMinValue.configure(text=int(self.slider_dia_min.get()))
+            self.slider_dia_max.configure(from_=minsize)
+            self.slider_dia_max.configure(to=maxsize)
+            self.slider_dia_max.set(maxsize)
+            self.lblMaxValue.configure(text=int(self.slider_dia_max.get()))  
+            self.SliderLabel.configure(text='-->')      
+        # Activate UI elements
+            self.chkSizeLimit.configure(state='normal')
+            self.btn_DetectViaas.configure(state='normal')
 
     def slider_min_event(self, slidervalue):
-        self.lblMinValue.configure(text=slidervalue)
+        self.lblMinValue.configure(text=int(slidervalue))
     
     def slider_max_event(self, slidervalue):
-        self.lblMaxValue.configure(text=slidervalue)
+        self.lblMaxValue.configure(text=int(slidervalue))
 
     def chkbox_SizeLimit(self):
         if self.chkSizeLimit.get():
--- test1.py
+++ test1.py
@@ -6,29 +6,34 @@ import matplotlib.pyplot as plt
 from PIL import Image
 import pymsgbox
 from numpy.polynomial import polynomial as P
+import math
+
 
 #VAriable
 debug = True
 via_dia = 0
 cnt_vias_in_row = 0 
 
+
 def regen_image(minArea=15000, maxArea=25000):
+    global via_dia, thresh, img
     # Find contours in the image
     contours, _ = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     areas =[]
     cx =[]
     cy = []
     df = pd.DataFrame()
+    # Convert thresh image into RGB to display with contours draws
+    disp_image = cv2.cvtColor(thresh, cv2.COLOR_GRAY2BGR)
 # Iterate through the contours
     for cnt in contours:
 # Find the area of the contour
-        area = cv2.contourArea(cnt)
-        
+        area = cv2.contourArea(cnt)       
 # Check if the contour has a round shape
-        if ((area > minArea) & (area < maxArea)):
+        if ((area >= minArea) & (area < maxArea)):
             areas.append(area)
 # Draw the contour on the image
-            cv2.drawContours(img, [cnt], 0, (0, 255, 0), 12)
+            cv2.drawContours(disp_image, [cnt], 0, (255, 0, 0), 12)
             M = cv2.moments(cnt)
             #print( M )
             cx.append(int(M['m10']/M['m00']))
@@ -41,9 +46,39 @@ def regen_image(minArea=15000, maxArea=25000):
     avrg_dia = np.mean(areas)
     via_dia = 2*np.sqrt(avrg_dia/np.pi)
     if debug:
-        show_image(img,1024*4)
+        show_image(disp_image,1024*4)
         print(via_dia)
     return df
+
+def find_min_max_size(graymin=127, graymax=255):
+    global via_dia, thresh, img
+    file_path = filedialog.askopenfilename()
+    if file_path=="":
+        return
+    img = cv2.imread(file_path)
+# Convert the image to grayscale
+    gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# Threshold the image to convert it to binary
+    ret, thresh = cv2.threshold(gray_image, graymin, graymax, cv2.THRESH_BINARY)
+#r Apply Gaussian Blur Filter
+    #thresh = cv2.GaussianBlur(thresh, (15, 15), 0)
+# Find contours in the image
+    # contours, _ = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    contours, _ = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+    areas =[]
+    cx =[]
+    cy = []
+    df = pd.DataFrame()
+
+    disp_image = cv2.cvtColor(thresh, cv2.COLOR_GRAY2BGR)
+# Iterate through the contours
+    for cnt in contours:
+# Find the area of the contour
+        area = cv2.contourArea(cnt)
+        if area > 100:
+            areas.append(area)
+    return math.ceil(np.min(areas)), math.ceil(np.mean(areas)), math.ceil(np.max(areas))
+
 
 def find_circ(minArea):
     global via_dia, thresh, img
@@ -140,8 +175,8 @@ def data_process(df):
     row_vias_count = {i:list_df_len.count(i) for i in list_df_len}
     if debug:
         pass
-        # print(row_vias_count)
-        # print(row_vias_count.keys())
+        print(row_vias_count)
+        print(row_vias_count.keys())
         #print(df_list[(len(df_list)-1)])
     return df_list, row_vias_count
 
@@ -194,14 +229,19 @@ def plot_distortions(list_of_rows):
 
 def main():
     global cnt_vias_in_row
-    df2 = find_circ(15000)
+    mymin,mymean,mymax = find_min_max_size()
+    print(str(mymin) + " ; " + str(mymean) + " ; " + str(mymax))
+    df2 = regen_image(mymin,mymax)
+    print(df2)
+    # return
+    # df2 = find_circ(15000)
     #return
-    pd.set_option('display.max_rows', 500)
+    #pd.set_option('display.max_rows', 500)
     rows_list, rows_vias_cnt = data_process(df2)
-    cnt_vias_in_row = max(rows_vias_cnt, key=rows_vias_cnt.get)
+    cnt_vias_in_row = max(rows_vias_cnt.keys())
 #--- Check if there is different vias count in the rows and choose the largest vias count
     if len(rows_vias_cnt)>1:
-        cnt_vias_in_row = max(rows_vias_cnt, key=rows_vias_cnt.get)
+        #cnt_vias_in_row = max(rows_vias_cnt, key=rows_vias_cnt.get)
         pymsgbox.alert(f'There are rows with diferent vias counts! \n vias_num:rows_count {rows_vias_cnt}' +
         '\n Only rows with largest vias count will be selected', 'Title')
     poly_args = plot_distortions(rows_list)
